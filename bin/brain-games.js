c7693b05cfa46a7cfd71d94ad8b676123a3ad10e
wasd21xd <Avey21703@gmail.com>
2025-04-29 15:00:00 +0300

    v1.0.1

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,10 @@
 #!/usr/bin/env node
+import readlineSync from 'readline-sync'
+
 
 
 console.log('Welcome to the Brain Games!');
+
+
+const name = readlineSync.question('what your name? ');
+console.log(`Hello, ${name}!`)
